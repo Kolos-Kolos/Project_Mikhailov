42f03d1d0b37f70cb7d35cbca0dd16c9a516a20b
Kolos-Kolos <romamixaylov2008@mail.ru>
2026-06-06 15:48:26 +0300

    PZ17.1.py

--- PZ17/PZ17.1.py
+++ PZ17/PZ17.1.py
@@ -1,76 +1,25 @@
-# ПЗ №17
-# Вариант 16
-# Форма регистрации на мастер-класс
+• Найдите все натуральные числа (возможно, окружённые буквами);
+\d+
+• Найдите все «слова», написанные капсом (то есть строго заглавными), возможно внутри настоящих слов (аааБББввв);
+​[A-ZА-ЯЁ]+
+• Найдите слова, в которых есть русская буква, а за ней цифра;
+​\b\w*[а-яА-ЯёЁ]\d\w*\b
+• Найдите все слова, начинающиеся с русской или латинской большой буквы (\b — граница слова);
+​\b[A-ZА-ЯЁ]\w*
+• Найдите слова, которые начинаются на гласную (\b — граница слова);
+​\b[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]\w*
+• Найдите все натуральные числа, не находящиеся на границе слова;
+​\B\d+\B
+• Найдите строчки, в которых есть символ * (. — это точно не конец строки!);
+​^.*\*.*$
+• Найдите строчки, в которых есть открывающая и когда-нибудь потом закрывающая скобки;
+​^.*\(.*\).*$
+• Выделите одним махом весь кусок оглавления (в конце примера, вместе с тегами);
+​<[^>]+>[\s\S]*?<\/[^>]+>
+• Выделите одним махом только текстовую часть оглавления, без тегов;
+​(?<=>)[^<>]+(?=<)
+• Найдите пустые строчки;
+​​^[ \t]*$\r?\n?
+• Найти все теги, не включая их содержимое.
+​<[^>]+>
 
-from tkinter import *
-
-
-def submit():
-    print("Регистрация выполнена")
-    print("Имя:", name_entry.get())
-    print("Фамилия:", surname_entry.get())
-    print("Email:", email_entry.get())
-    print("Телефон:", phone_entry.get())
-
-
-window = Tk()
-window.title("Workshop Registration")
-window.geometry("500x400")
-
-# Заголовок
-title_label = Label(
-    window,
-    text="Workshop Registration",
-    font=("Arial", 16)
-)
-title_label.pack(pady=10)
-
-# Имя
-Label(window, text="First Name").pack()
-name_entry = Entry(window, width=40)
-name_entry.pack()
-
-# Фамилия
-Label(window, text="Last Name").pack()
-surname_entry = Entry(window, width=40)
-surname_entry.pack()
-
-# Email
-Label(window, text="Email").pack()
-email_entry = Entry(window, width=40)
-email_entry.pack()
-
-# Телефон
-Label(window, text="Phone").pack()
-phone_entry = Entry(window, width=40)
-phone_entry.pack()
-
-# Пол
-Label(window, text="Gender").pack()
-
-gender = StringVar()
-gender.set("Male")
-
-Radiobutton(
-    window,
-    text="Male",
-    variable=gender,
-    value="Male"
-).pack()
-
-Radiobutton(
-    window,
-    text="Female",
-    variable=gender,
-    value="Female"
-).pack()
-
-# Кнопка регистрации
-Button(
-    window,
-    text="Register",
-    command=submit
-).pack(pady=15)
-
-window.mainloop()
-```
